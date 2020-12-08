96d4d2c5c59d8a25f474e299662b1cd7497218a8
Matt Dudzik <mdudzik92@gmail.com>
2020-12-08 16:51:27 -0600

    Fixed api calls, looped the array inside the object in reponse, appended 5 day forecast cards to searchForcast function

--- script.js
+++ script.js
@@ -1,100 +1,62 @@
-$(document).ready(function () {
-	$("#search").on("click", function () {
-		var cityName = $("#city").val()
-		console.log(cityName)
-		searchWeather(cityName)
-		searchForecast(cityName)
-	})
+var city = $("#cityChoice").val();
+var key = "&appid=8439103e642ad3d44f7bafaa0e94e84f";
 
+$("#searchButton").on("click", function () {city = $("#cityChoice").val();
+	$("#cityChoice").val("");
+	$.ajax({url:"https://api.openweathermap.org/data/2.5/weather?q= " + city + "&units=imperial" + key, type: "GET"}).then(function (data) {
+		console.log(data);
+		listCities();
+		searchWeather(data);
+		searchForecast(data);
+	});
+});
 
+function listCities() {
+	var listItem = $("<li>").addClass("list-group-item").text(city);
+	$(".list").append(listItem);
+}
 
-	function searchWeather(citySearch) {
-		$.ajax({
-			url: "http://api.openweathermap.org/data/2.5/weather?q=" + citySearch + "&appid=8439103e642ad3d44f7bafaa0e94e84f&units=imperial",
-			dataType: "json",
-			success: function (data) {
-				var temp = data.main.temp;
-				console.log(data.main.temp)
-				var humidity = data.main.humidity;
-				console.log(data.main.humidity)
-				var speed = data.wind.speed;
-				console.log(data.wind.speed)
-				var img = $("<img>").attr("src", "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png");
-				var newCard = `<div class="card" style="width: 18rem;">
-				<div class="card-header">
-				  ${citySearch}
-			
-				</div>
-				<ul class="list-group list-group-flush">
-				  <li class="list-group-item">Temp: ${temp}</li>
-				  <li class="list-group-item">Humidity: ${humidity}</li>
-				  <li class="list-group-item">Wind Speed: ${speed}</li>
-				</ul>
-			  </div>`
-				var weather = $("today");
-				console.log(weather)
-				$("#today").empty()
-				$("#today").append(img, newCard)
-				// show the data on the DOM
+function searchWeather(data) {
+	var date = new Date();
+	$("#citySearch").empty();
+	var card = $("<div>").addClass("card");
+	var cardBody = $("<div>").addClass("card-body");
+	var city = $("<h4>").addClass("card-title").text(data.name);
+	console.log(data.name);
+	const cityDate = $("<h4>").addClass("card-title").text(date.toLocaleDateString("en-US"));
+	var image = $("<img>").attr("src", "https://openweathermap.org/img/w/" + data.weather[0].icon + ".png");
+	console.log(data.weather[0].icon);
+	var temp = $("<p>").addClass("card-text current-temp").text("Temperature: " + data.main.temp + " °F ");
+	var humidity = $("<p>").addClass("card-text current-humidity").text(" Humidity: " + data.main.humidity + "%");
+	console.log(data.main.humidity);
+	var windSpeed = $("<p>").addClass("card-text current-wind-speed").text(" Wind Speed : " + data.wind.speed + "MPH");
+	console.log(data.wind.speed);
+	(lat = data.coord.lat), (lon = data.coord.lon);
+	$.ajax({
+		url:"https://api.openweathermap.org/data/2.5/uvi?lat=" + lat + "&lon=" + lon + key, method: "GET"}).then(function (data) {console.log(data.value);
+		var uvIndex = $("<p>").addClass("card-text current-uv").text(" UV index : " + data.value);
+		city.append(cityDate, image); 
+		cardBody.append(city, temp, humidity, windSpeed, uvIndex);
+		card.append(cardBody);
+		$("#citySearch").append(card)});
+}
+
+function searchForecast() {
+	$.ajax({url:"https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial" + key, method: "GET"}).then(function (data) {console.log(data.list);
+		var date = new Date();
+		$("#forecast").empty();
+		for (var i = 0; i < data.list.length; i++) {
+			if (data.list[i].dt_txt.indexOf("12:00:00") !== -1) {
+				var card = $("<div>").addClass("card col-md-2 ml-4 bg-dark text-white");
+				var cardBody = $("<div>").addClass("card-body p-3 forecastBody");
+				var cityDate = $("<h4>").addClass("card-title").text(moment(data.list[i].dt, "X").format("MMM Do"));
+				var temp = $("<p>").addClass("card-text forecastTemp").text("Temp: " + data.list[i].main.temp + "°F ");
+				var humidity = $("<p>").addClass("card-text forecastHumidity").text("Humidity: " + data.list[i].main.humidity + "%");
+				var image = $("<img>").attr("src", "https://openweathermap.org/img/w/" + data.list[i].weather[0].icon + ".png");
+				cardBody.append(cityDate, image, temp, humidity);
+				card.append(cardBody);
+				$("#forecast").append(card);
 			}
-		})
-	}
-
-
-
-	function searchForecast(citySearch) {
-		$.ajax({
-			url: "http://api.openweathermap.org/data/2.5/forecast?q=" + citySearch + "&appid=8439103e642ad3d44f7bafaa0e94e84f&units=imperial",
-			dataType: "json",
-			success: function (data) {
-				// console.log(data.list[0].dt_txt)
-				console.log(data)
-
-				for (var i = 0; i < data.list.length; i = i+8) {
-					console.log(data)
-				
-				// 	let curr = data.list[i]
-				// 	var date = curr.dt_txt;
-				// 	// var img = $("<img>").attr("src", "http://openweathermap.org/img/w/" + curr.weather[0].icon + ".png");
-				// 	var temp = curr.temp;
-				// 	var humidity = curr.humidity;
-				// 	var speed = curr.wind.speed;
-				// 	console.log(humidity);
-				// 	console.log(speed);
-					
-				// 	var newCard = `<div class="card" style="width: 18rem;">
-				// 	<div class="card-header">
-				// 	  ${citySearch}
-				
-				// 	</div>
-				// 	<ul class="list-group list-group-flush">
-				// 	  <li class="list-group-item">Date: ${date}</li>
-				// 	  <li class="list-group-item">Temp: ${temp}</li>
-				// 	  <li class="list-group-item">Humidity: ${humidity}</li>
-				// 	</ul>
-				//   </div>`
-				// 	$("#forecast").append(newCard)
-				}
-				
-			}
-		})
-
-	}
-
-	// perpare the url that is different
-	// is returning and array of objects
-	// loop the array inside the object in the response of the ajax call and append the card to the other section
-
-
-	// function searchWeather(citySearch) {
-	// 	$.ajax({
-	// 		url: "http://api.openweathermap.org/data/2.5/weather?q=" + citySearch + "&appid=8439103e642ad3d44f7bafaa0e94e84f",
-	// 		dataType: "json",
-	// 		success: function (data) {
-	// 			console.log(data)
-	// 		}
-	// 	})
-	// }
-
-})
-
+		}
+	});
+}
